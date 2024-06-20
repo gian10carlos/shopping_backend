f50e18b5e51a715f62ca3be002f2ad0d15563581
gian10carlos <gc2130925@gmail.com>
2024-06-20 12:45:03 -0500

    feat: updated file connection

--- db/connection.js
+++ db/connection.js
@@ -1,22 +1,12 @@
-const mysql = require('mysql')
-const dotenv = require('dotenv')
-
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = require('./config');
+const { createPool } = require('mysql2');
 
-dotenv.config({ path: './.env' });
-
-const connection = mysql.createConnection({
+const connection = createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_DATABASE,
     port: DB_PORT,
-});
+})
 
-connection.connect((err) => {
-    if (err) {
-        throw 'Error connection database', err;
-    }
-    console.log('Connection successful database');
-});
 module.exports = connection;
