ad5c15858e13326cea9cde641bf176183db74f1f
gian10carlos <gc2130925@gmail.com>
2024-06-20 16:59:45 -0500

    feat: rename db config

--- db/config.js
+++ db/config.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USER = process.env.DB_USER || "root";
 const DB_PASSWORD = process.env.DB_PASSWORD || "";
-const DB_DATABASE = process.env.DB_DATABASE || "pinoza_store";
+const DB_NAME = process.env.DB_NAME || "pinoza_store";
 const DB_PORT = process.env.DB_PORT || 3306;
 
-module.exports = { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT }
+module.exports = { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT }
--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const connection = createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    database: DB_DATABASE,
+    database: DB_NAME,
     port: DB_PORT,
 });
 
