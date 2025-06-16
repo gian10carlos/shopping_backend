2bc03f47c792a8d11d7a6fce4ef8106a070d6027
Gian Carlos MR <115681632+gian10carlos@users.noreply.github.com>
2025-06-16 13:41:40 -0500

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,11 +1,1 @@
-const dotenv = require('dotenv');
-dotenv.config();
 
-const PORT = process.env.PORT || 3005;
-const DB_HOST = process.env.DB_HOST || "viaduct.proxy.rlwy.net";
-const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "urtCuEECTXtVzvWmftoCWvxTzPwyysXE";
-const DB_NAME = process.env.DB_NAME || "railway";
-const DB_PORT = process.env.DB_PORT || 49064;
-
-module.exports = { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT }
